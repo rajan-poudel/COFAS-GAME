03c70973f5346cc1c762d67541bfea0b51a6ab63
Rajan Poudel <117752332+r-a-j-a-n@users.noreply.github.com>
2024-10-02 15:52:40 +0545

    Done

---
+++ Code/README.md
@@ -0,0 +1,2 @@
+# Cyber-Ninza
+A game made for COFAS International Computer Olympiad by Ankuram Academy(Rajan&Sajan)
---
+++ Code/sound_effects.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.mixer.init()
+
+gameover = pygame.mixer.Sound('Music/gameover.mp3')
+fire = pygame.mixer.Sound('Music/fire.mp3')
+levelup = pygame.mixer.Sound('Music/levelup.mp3')
